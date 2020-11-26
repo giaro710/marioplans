af7a9ba1598db7ae8cc9553944ecbf89beae35bf
Gian Marco Romani <giaro710@gmail.com>
2020-11-26 12:17:53 +0100

    Done projectSummary and Details components

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ProjectDetails = (props) => {
+  const id = props.match.params.id;
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project title - {id}</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, iste.
+            Inventore quam maiores dicta fugiat tenetur unde aliquam aut autem
+            fugit error! Nam beatae eaque saepe asperiores minus voluptate ea.
+          </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by someone</div>
+          <div>2nd september, 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
