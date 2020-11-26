9ba9158c09b29ed8acb0638d65353049385d0eb7
Gian Marco Romani <giaro710@gmail.com>
2020-11-26 11:08:36 +0100

    started setting up the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import Navbar from "./components/layouts/Navbar";
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>Marioplans</h1>
+          <Navbar />
         </div>
       </BrowserRouter>
     );
